39f75bae5636c3ae1535d3e293035c3f9458de47
Big_Bread <38143572+staierb@users.noreply.github.com>
2021-11-10 14:21:02 +0000

    cleaned up the unused code

--- main.py
+++ main.py
@@ -10,25 +10,10 @@ for lin in hand:
   wds = lin.split()
   #print (wds)
   for w in wds:
-    # if the key is not there the count is zero
-    #print('##', w,di.get(w,-99))
-    #oldcount = di.get(w,0)
-    #print(w, 'old',oldcount)
-    #newcount= oldcount + 1
-    #di[w] = newcount
-    #print(w, 'new',newcount)
-    #this replaces the above code with one line
+   
     #idiom: retrieve/create/update counter
     di[w] = di.get(w,0) + 1
-    #print(w, 'new',di[w])
-    #if w in di:      
-      #di[w] = di[w] + 1
-      #print ('@Existing@')
-    #else:
-      #di[w] = 1
-      #print ('@new@')
-    #print(w, di[w])
-#print (di)
+    
 #now we want to find the most common word
 largest = -1
 theword = None
@@ -36,5 +21,7 @@ for k,v in di.items():
  # print (k,v)
   if v > largest:
     largest = v
-    theword = k #capture/remember the largest word
+    #capture/remember the largest word
+    theword = k     
 print (theword, largest)
+
